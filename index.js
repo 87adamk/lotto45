3f45607bd6edbb6adc1a6afddba246018befc36d
OPENSNS 김수민 <smkim87@opensns.co.kr>
2016-11-11 10:28:03 +0900

    no message

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/main');
+  response.render('pages/main2');
 });
 
 app.listen(app.get('port'), function() {
