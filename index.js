2467fd49931aa69ae4dc39d038127d4150376a85
OPENSNS 김수민 <smkim87@opensns.co.kr>
2016-11-11 13:23:17 +0900

    no message

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var cheerio = require('cheerio');
 var request = require('request');
+var url = 'http://blog.saltfactory.net';
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,24 +14,22 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.render('pages/main');
+ 	res.render('pages/main');
 });
 
 app.get('/test', function(req, res) {
-  	
-	var url = 'http://blog.saltfactory.net';
-	request(url, function(error, res, html){
+	res.render('pages/index');
+});
+
+request(url, function(error, res, html){
 		if (error) {throw error};
 
 		console.log (html);
 
 	});
 
-	res.render('pages/index');
-});
-
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
 
 
