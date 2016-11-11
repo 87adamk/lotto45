673bac9f65e216bac8b9fb4e5303358dc71f5255
OPENSNS 김수민 <smkim87@opensns.co.kr>
2016-11-11 18:07:38 +0900

    bug fix

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function getHistory() {
 			
 			var $ = cheerio.load(iconv.convert(html).toString('utf-8'));
 					
-			$("table.tblType1 > tbody > tr:gt(0):lt(6) > td:nth-child(2)").foreach(function(){
+			$("table.tblType1 > tbody > tr:gt(0):lt(6) > td:nth-child(2)").each(function(){
 				msg = msg + $(this).text();
 			})
 		})
