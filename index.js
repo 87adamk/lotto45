a13b8582b60b2849f930aa9396e289dca1cb0e04
OPENSNS 김수민 <smkim87@opensns.co.kr>
2016-11-11 10:33:42 +0900

    qrcode.js 변경

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/main2');
+  response.render('pages/main');
 });
 
 app.listen(app.get('port'), function() {
